95083e91de2bda51e9849517c794ab52ecb99191
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-06-26 15:52:13 -0400

    about section render image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import coverImage from "../../assets/cover";
+import coverImage from "../../assets/cover/ZacnMolly.jpg";
 
 function About() {
   return (
     <section className="my-5">
       <h1 id="about">Zac Eitel</h1>
+      <p>Web Developer</p>
       <img
         src={coverImage}
         className="my-2"
-        style={{ width: "100%" }}
+        style={{ width: "90%" }}
         alt="cover"
       />
     </section>
